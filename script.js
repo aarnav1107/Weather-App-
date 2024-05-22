6475ebdb7b4eb7d431a5ffcf57c1513462b5a131
Aarnav Singh <aarnavsingh.11@gmail.com>
2024-05-22 19:57:55 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,28 +1,82 @@
-const button = document.querySelector('#search-button');
-const input = document.querySelector('#city-input');
-const cityName = document.querySelector('#city-name');
-const cityTemp = document.querySelector('#city-temp');
-const cityTime = document.querySelector('#city-time');
-async function getWeather(city) {
-    const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=611a2d322546468cb5750102242205&q=${city}&aqi=yes`);
-    // const data = await response.json();
-    // console.log(data);
+// //event handling
+// function myFunction()
+// {
+//     console.log("I am clicked");
+// }
+// const mouseover = () => {
+//     console.log('mouse over');
+// }
+// function keyPressEvent() {
+//     console.log('key was pressed');
+// }
 
-return await response.json();
+// function keyUpEvent() {
+//     console.log('key up was pressed');
+// }
+
+// function keyDownEvent() {
+//     console.log('key down was pressed');
+// } 
+
+
+// //event listners
+// const box1 = document.getElementById('box-1');
+// box1.addEventListener('mousemove', (e) => {
+//     console.log('event object', e.clientX, e.clientY);
+// })
+
+// const nameInput = document.getElementById('nameInput');
+// nameInput.addEventListener('keydown', (e) => {
+//     console.log('key', e.key);
+// })
+
+// nameInput.addEventListener('focus', (e) => {
+//     console.log('key', e);
+// // }) 
+
+
+
+// const box4=document.getElementById('box-4');
+// box4.addEventListener('click',()=>{
+//     console.log('box-4 was clicked');
+// }) 
+
+
+// box2.adEventListne('keydow',()=>{
+
+// })
+
+Array.prototype.extra="extraone";
+const arr = [1,2,3,4];
+arr.forEach((element)=>{
+    console.log(element);
+})
+//apply for in loop
+for(let index in arr){
+    console.log(index);
+    //console.log(arr[index]);
 }
+// //has own property
+// for(let index in arr){
+//     if(arr.hasOwnProperty(index)){
+//         console.log(index);
+//     }
+// }
+// //apply for of loop
+// for(let element of arr){
+//     console.log(element);
+// }
+//apply for each loop
+// arr.forEach((element)=>{
+//     console.log(element);
+// })
 
-
-
-button.addEventListener('click',async () => {
-    //console.log(input.value);
-    const value = input.value;
-    const result=await getWeather(value);
-   // console.log(result);
-   cityName.innerText = `${result.location.name},${result.location.region}-${result.location.country}`
-    cityTemp.innerText = `${result.current.temp_c}°C`
-    cityTime.innerText = `${result.location.localtime}`
-    document.getElementById('icon').src = data.current.condition.icon;
-
-//http://api.weatherapi.com/v1/current.json?key=611a2d322546468cb5750102242205&q=London&aqi=yes 
-
-});
+Array.prototype.customForEachOne = function(callback){
+    for(let i=0;i<this.length;i++){
+        callback(this[i],i,this);
+    }
+}
+// using for each loop
+// arr.customForEachOne((element,index,arr)=>{
+//     console.log(element);
+// })
